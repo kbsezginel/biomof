b22ee2f1a8b05001e8813f6f897c98820217bb32
kbsezginel <kbs37@pitt.edu>
2017-07-11 16:30:52 -0400

    Add ZIF-8 example files and UFF files for RASPA.

---
+++ raspa/example/ZIF-8/RFP/qsub_raspa.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#PBS -j oe
+#PBS -N ZIF-8-RFP
+#PBS -q idist_big
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=24:00:00
+#PBS -S /bin/bash
+
+echo JOB_ID: $PBS_JOBID JOB_NAME: $PBS_JOBNAME HOSTNAME: $PBS_O_HOST
+echo start_time: `date`
+
+## Load python virtual environment
+. /ihome/cwilmer/kbs37/venv/ipmof/bin/activate
+
+cd $PBS_O_WORKDIR
+simulate simulation.input
+
+echo end_time: `date`
+# workaround for .out / .err files not always being copied back to $PBS_O_WORKDIR
+cp /var/spool/torque/spool/$PBS_JOBID.OU $PBS_O_WORKDIR/$PBS_JOBID$(hostname)_$$.out
+cp /var/spool/torque/spool/$PBS_JOBID.ER $PBS_O_WORKDIR/$PBS_JOBID$(hostname)_$$.err
+
+exit
